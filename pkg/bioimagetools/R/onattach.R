8cc74b66b69f02f9bc4963f08a30c68875abfc50
volkerschmid <volkerschmid@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-07-22 08:55:34 +0000

    onattach: 0.2.x
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bioimagetools@64 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bioimagetools/R/onattach.R
+++ pkg/bioimagetools/R/onattach.R
@@ -1,4 +1,4 @@
 .onAttach<-function(libname, pkgname)
 {
-packageStartupMessage(message("Welcome to Bioimagetools\n","Version 0.2.7"))
+packageStartupMessage(message("Welcome to Bioimagetools\n","Version 0.2.x"))
 }
