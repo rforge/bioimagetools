d66066c2d3141d46228b217699a9fbc05877bf21
volkerschmid <volkerschmid@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-18 15:57:18 +0000

    Add EBImage morphology
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bioimagetools@81 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bioimagetools/R/onattach.R
+++ pkg/bioimagetools/R/onattach.R
@@ -1,4 +1,4 @@
 .onAttach<-function(libname, pkgname)
 {
-packageStartupMessage(message("Welcome to Bioimagetools\n","Version 0.2.15"))
+packageStartupMessage(message("Welcome to Bioimagetools\n","Version 0.2.16"))
 }
